5d63d4f4762e600ef06284b1455371e836482e6c
DimaMykhnevych <dmitriy.mykhnevych@gmail.com>
2021-02-04 12:33:00 +0200

    Add Select Disciplines By Students Feature

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using WebApplication1.Data;
@@ -20,9 +24,67 @@ namespace WebApplication1.Controllers
 
         public IActionResult Index()
         {
-            //int n = _db.Students.Count();
-            //return Content(n.ToString());
-            return View();
+            var studetns = _db.Students;
+            return View(studetns);
+        }
+
+        [HttpGet]
+        public IActionResult SelectDisciplines(int id)
+        {
+            var student = _db.Students.Include(s => s.Disciplines).SingleOrDefault(s => s.Id == id);
+            var disciplines = _db.Disciplines;
+            StudentDisciplinesViewModel studentDisciplines = new StudentDisciplinesViewModel
+            {
+                Disciplines = disciplines,
+                Student = student,
+            };
+            return View(studentDisciplines);
+        }
+
+        [HttpPost]
+        public IActionResult SelectDisciplines(int id, IFormCollection collection)
+        {
+            var selectedDisciplines = collection.Keys.ToList();
+            selectedDisciplines.RemoveAt(selectedDisciplines.Count - 1);
+
+            var allDisciplinesIds = _db.Disciplines.Select(d => d.Id.ToString()).ToList();
+            var uncheckedDisciplines = allDisciplinesIds.Except(selectedDisciplines).ToList();
+
+            var currentStudent = _db.Students.Include(s => s.Disciplines).SingleOrDefault(s => s.Id == id);
+
+            if (selectedDisciplines.Any())
+            {
+                AddStudentDiscipline(selectedDisciplines, currentStudent);
+            }
+
+            if (uncheckedDisciplines.Any())
+            {
+                DeleteStudentDiscipline(uncheckedDisciplines, currentStudent);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        private void AddStudentDiscipline(List<string> selectedDisciplines, Student currentStudent)
+        {
+            for (int i = 0; i < selectedDisciplines.Count; i++)
+            {
+                var disc = _db.Disciplines.SingleOrDefault(d =>
+                d.Id == Convert.ToInt32(selectedDisciplines[i]));
+                currentStudent.Disciplines.Add(disc);
+            }
+            _db.SaveChanges();
+        }
+
+        private void DeleteStudentDiscipline(List<string> uncheckedDisciplines, Student currentStudent)
+        {
+            for (int i = 0; i < uncheckedDisciplines.Count; i++)
+            {
+                var disc = _db.Disciplines.SingleOrDefault(d =>
+                d.Id == Convert.ToInt32(uncheckedDisciplines[i]));
+                currentStudent.Disciplines.Remove(disc);
+            }
+            _db.SaveChanges();
         }
 
         public IActionResult Privacy()
---
+++ WebApplication1/WebApplication1/Models/StudentDisciplinesViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace WebApplication1.Models
+{
+    public class StudentDisciplinesViewModel
+    {
+        public Student Student { get; set; }
+        public IEnumerable<Discipline> Disciplines { get; set; }
+    }
+}
