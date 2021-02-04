fea1dd4c29f860034e36cf63717895450d5cd00f
DimaMykhnevych <dmitriy.mykhnevych@gmail.com>
2021-02-04 09:51:21 +0200

    Add Select Helper

---
+++ WebApplication1/WebApplication1/Helpers/MyHelpers.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Helpers
+{
+    public static class MyHelpers
+    {
+        public static HtmlString Select<T, TVal>(this IHtmlHelper html, 
+            IEnumerable<T> xs, string valName, string txtName, TVal selected)
+        {
+            string selectMarkup = "<select class='form-control'>";
+            foreach(var item in xs)
+            {
+                Type itemType = item.GetType();
+                Object value = itemType.GetProperty(valName).GetValue(item);
+                Object text = itemType.GetProperty(txtName).GetValue(item);
+                if(selected.ToString() == value.ToString())
+                {
+                    selectMarkup += $"<option value='{value}' selected>{text}</option>\n";
+                }
+                else
+                {
+                    selectMarkup += $"<option value='{value}'>{text}</option>\n";
+                }
+            }
+            selectMarkup += "</select>";
+            return new HtmlString(selectMarkup);
+        }
+    }
+}
